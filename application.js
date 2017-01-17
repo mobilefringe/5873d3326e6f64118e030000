6eee22eb0ef31304abef333b488f6b0290a0d541
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-01-17 11:23:36 -0500

    Westbrook Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -59,6 +59,18 @@ function renderGeneral(container, template, collection){
     $(container).html(item_rendered.join(''));
 }
 
+function renderPropertyDetails(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    
+    var repo_rendered = Mustache.render(template_html, collection);
+    item_rendered.push(repo_rendered);
+
+    $(container).html(item_rendered.join(''));
+}
+
 function renderBanner(home_banner_template, home_banner, banners){
     var item_list = [];
     var item_rendered = [];
